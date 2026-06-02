dcc49f3f4b7a02eeefdc2e66d38d1febfb5d3111
GLB25116CN380 <pankhoodeeparee@gmail.com>
2026-06-02 21:27:56 +0530

    Add files via upload

---
+++ day1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int n,sum ;
+    printf("enter the value of number n\n");
+    scanf("%d", &n);
+    sum = n*(n+1)/2;
+    printf("the sum of number: %d",sum);
+    return 0;
+}
