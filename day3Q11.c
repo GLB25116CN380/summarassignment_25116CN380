e5fb19471559b701399412f32536efb2ba51468d
GLB25116CN380 <pankhoodeeparee@gmail.com>
2026-06-05 10:08:09 +0530

    Add files via upload

---
+++ day3Q11.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ int main(){
+  int num1,num2,gcd;
+  printf("enter the two numbers ");
+  scanf("%d%d", &num1, &num2);
+  for(int i=1; i<=num1,&num2;i++){
+    if(num1%1==0 && num2%i==0)
+    gcd=i
+  }
+  printf("gcd of %d and %d is %d",num1,num2);
+    return 0;
+}
