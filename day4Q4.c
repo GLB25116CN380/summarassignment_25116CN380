2887d5c0e0b24cfbddddee2f6f4e482070d9ff1a
GLB25116CN380 <pankhoodeeparee@gmail.com>
2026-06-06 19:29:29 +0530

    Add files via upload

---
+++ day4Q4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int min,max,n,r,sum,armstrong,i;
+    printf("enter min and max value");
+    scanf("%d %d", &min, &max);
+    for(i=min; i<=max;i++){
+        n=i;
+        armstrong=n;
+        while(n!=0){
+            r=n%10;
+            sum=sum+r*r*r;
+            n=n/10;
+        }
+
+    }
+      if(sum==armstrong)
+      printf("%d\n",sum);
+    return 0;
+
+}
