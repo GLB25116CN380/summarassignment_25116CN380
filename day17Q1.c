ddef3c725b11c155dff376689208c72ad8a7d45b
GLB25116CN380 <pankhoodeeparee@gmail.com>
2026-06-17 19:51:20 +0530

    Add files via upload

---
+++ day17Q1.c
@@ -0,0 +1,23 @@
+
+
+
+#include<stdio.h>
+ void main(){
+    int arr[10],arr2[10],mergedarray[20],i,j;
+    printf("enter 10 numbers for array1\n ");
+    for(i=0;i<10;i++){
+      scanf("%d", &arr[i]);
+      mergedarray[i]=arr[i];
+    }
+    printf("enter 10 numbers for array2\n");
+    for(j=0;j<10;j++){
+      scanf("%d", &array2[j]);
+      mergedarray[=arr2[j];
+      i++;  
+ } 
+   printf("merged array\n");
+   for(j=0;j<20;j++){
+    printf("%d",mergedarray[j]);
+   }
+  return 0;
+ }    
