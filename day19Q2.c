f7836fd29c9ea3216c18e7cd9b3c7b53151b741e
GLB25116CN380 <pankhoodeeparee@gmail.com>
2026-06-19 20:59:41 +0530

    Add files via upload

---
+++ day19Q2.c
@@ -0,0 +1,28 @@
+
+
+#include<stdio.h>
+int main(){
+  int m1[2][2],m2[2][2],m3[3][3],r,c;
+  printf("enter 1st matrix element\n");
+       for(r=0;r<3;r++){
+        for(c=0;c<3;c++){
+         
+          scanf("%d",&m1[r][c]);
+        }
+       }
+       printf("enter 2nd  matrix elements");
+       for(r=0;r<3;r++){
+         for(c=0;c<3;c++){
+          scanf("%d",&m2[r][c]);
+         }
+       }
+       printf("subtraction of two matrices is\n");
+       for(r=0;r<3;r++){
+        for(c=0;c<3;c++){
+          m3[r][c]=m1[r][c]-m2[r][c];
+          printf("%d",m3[r][c]);
+        }
+        printf("\n");
+       }
+
+}
